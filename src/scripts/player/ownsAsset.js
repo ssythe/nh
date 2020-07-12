ad3d22a5e06eef27eda8aeb31f980cd5bf7a65d4
Jefemy <jefemyyt@gmail.com>
2020-07-12 03:36:18 +0000

    Fix ownsAsset function

--- src/scripts/player/ownsAsset.js
+++ src/scripts/player/ownsAsset.js
@@ -1,14 +1,19 @@
 const phin = require("phin")
     .defaults({ parse: "json", timeout: 12000 })
 
-const API = (assetId) => `https://api.brick-hill.com/v1/shop/owners?id=${assetId}`
+const API = (assetId, cursor) => `https://api.brick-hill.com/v2/shop/owners?id=${assetId}&limit=100&cursor=${cursor}`
 
 async function playerOwnsAsset(userId, assetId) {
-    const data = (await phin({url: API(assetId)})).body
+    let cursor = '';
+    while(cursor !== null) {
+        const body = (await phin({url: API(assetId, cursor)})).body
 
-    for (let copy of data) {
-        if (copy.owner === userId)
-            return true
+        for (let copy of body.data) {
+            if (copy.user_id === userId)
+                return true
+        }
+
+        cursor = body.next_cursor
     }
 
     return false
